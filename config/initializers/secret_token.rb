6cfbeda21e5942500fdb56a26907fcd59c27f5ea
Suke211 <skj.pass.2011@gmail.com>
2014-02-23 15:41:22 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HitSns::Application.config.secret_key_base = '146961cd276c675151bf50f143842d3ce117d542f80d02c39b0edbfe8cc6e2f7a5d36f153bbbc9847f109c0e79eff12688f755b72ed10b5056e15186ac55d6c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HitSns::Application.config.session_store :cookie_store, key: '_hit_sns_session'
